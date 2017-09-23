a9a5484ea4713292342c59ba705b9f8392af72eb
Easy89 <31477513+Easy89@users.noreply.github.com>
2017-09-22 21:04:42 -0500

    intial commit

---
+++ my-project/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+//import ProfileButton from 'ProfileButton.js'
+var ProfileButton = require('./ProfileButton');
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ProfileButton/>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'pink',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+
+
+
+
