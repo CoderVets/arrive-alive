c60e5117134f4c8d4726a4889fe50937e32e22d3
dodgeball01 <dodgeball01@gmail.com>
2017-11-07 22:59:00 -0600

    Files for Testing Lyft API

---
+++ AA_app/APImodules.js
@@ -0,0 +1,5 @@
+import React, { Component } from 'react';
+import {
+
+} from 'react-native'
+
---
+++ AA_app/LyftAPITest.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import {
+  ActivityIndicator,
+  ListView,
+  Text,
+  View
+} from 'react-native';
+
+export default class LyftAPITest extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            isloading: true
+        }
+    }
+
+    componentDidMount() {
+        return fetch('https://api.lyft.com/oauth/token',{
+            method: 'Post',
+            headers:{
+                'Content-Type':'application/json',
+                'OaJiui9NK0hS':'SANDBOX-VNDXMeFb-qwyMsEzZ-r5e82AzBtIg0ri'
+            },
+            body:JSON.stringigy({
+                "grant_type":"client_credentials",
+                "scope":"public"
+            })
+        });
+        .then((response) => response.json())
+        .then((responseJson) => {
+            let ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !==
+                r2});
+            this.setState({
+                isloading: false,
+                dataSource: ds.cloneWithRows(responseJson.token),
+            }, function() {
+                // do somthing with new stat
+            });
+        })
+    }
+}
